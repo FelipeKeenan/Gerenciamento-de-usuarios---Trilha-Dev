5736b6ad390bbe856d641024653e3da93b8a3406
felipeKeenan <felipekeenan2000@gmail.com>
2025-01-23 21:46:11 -0300

    Created DOM input variables

---
+++ index.js
@@ -0,0 +1,8 @@
+let name = document.querySelector('#exampleInputName')
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked') //Variável que irá armazenar o input radio que está selecionado
+let birth = document.querySelector('#exampleInputBirth')
+let country = document.querySelector('#exampleInputCountry')
+let email = document.querySelector('#exampleInputEmail')
+let password = document.querySelector('#exampleInputPassword')
+let photo = document.querySelector('#exampleInputFile')
+let admin = document.querySelector('#admin')
