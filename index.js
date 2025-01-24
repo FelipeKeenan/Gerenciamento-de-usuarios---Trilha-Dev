5db052a8cee7cea348ad234a2aaebbaec3b27c8c
felipeKeenan <felipekeenan2000@gmail.com>
2025-01-23 23:37:01 -0300

    Added submit event on form and used method preventDefault

--- index.js
+++ index.js
@@ -2,21 +2,28 @@
 
 let fields = document.querySelectorAll('#form-user-create [name]')
 let user = {} //Criando o objeto que terá as informações dinamicamente preenchidas no formulário
+let form = document.querySelector('#form-user-create')
 
 
-fields.forEach((field) => {
+console.log(fields)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
+    fields.forEach((field) => {
 
-    //Criando uma condição e selecionando apenas o input radio que está checked
-    if (field.name === 'gender' && field.checked) {
+        //Criando uma condição e selecionando apenas o input radio que está checked
+        if (field.name === 'gender') {
 
-        //Adiciona ou atualiza uma propriedade no objeto User
-        user[field.name] = field.value
+            if (field.checked) {
 
-    } else {
-        user[field.name] = field.value
-    }
+                //Adiciona ou atualiza uma propriedade no objeto User
+                user[field.name] = field.value
+            }
+
+        } else {
+            user[field.name] = field.value
+        }
+    })
 
     console.log(user)
-
-})  
+})
