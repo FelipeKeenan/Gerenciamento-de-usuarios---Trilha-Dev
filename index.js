40cb2a5b9f0da76d16bcc73b9484995ae7e93a89
felipeKeenan <felipekeenan2000@gmail.com>
2025-01-23 23:03:49 -0300

    Removed all variables and created a loop that runs through the form. Created a user variable, which will receive the values selected in the form and store them in an object.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let name = document.querySelector('#exampleInputName')
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked') //Variável que irá armazenar o input radio que está selecionado
-let birth = document.querySelector('#exampleInputBirth')
-let country = document.querySelector('#exampleInputCountry')
-let email = document.querySelector('#exampleInputEmail')
-let password = document.querySelector('#exampleInputPassword')
-let photo = document.querySelector('#exampleInputFile')
-let admin = document.querySelector('#admin')
+//Selecionando os elementos do document sem criar variáveis:
+
+let fields = document.querySelectorAll('#form-user-create [name]')
+let user = {} //Criando o objeto que terá as informações dinamicamente preenchidas no formulário
+
+
+fields.forEach((field) => {
+
+
+    //Criando uma condição e selecionando apenas o input radio que está checked
+    if (field.name === 'gender' && field.checked) {
+
+        //Adiciona ou atualiza uma propriedade no objeto User
+        user[field.name] = field.value
+
+    } else {
+        user[field.name] = field.value
+    }
+
+    console.log(user)
+
+})  
