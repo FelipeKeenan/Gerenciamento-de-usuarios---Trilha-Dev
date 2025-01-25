b305946e5cbe021d8808dc9c89cf01f36cf0b767
felipeKeenan <felipekeenan2000@gmail.com>
2025-01-24 21:03:51 -0300

    created the addLine function that will insert the line with the form information into the document

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ let fields = document.querySelectorAll('#form-user-create [name]')
 let user = {} //Criando o objeto que terá as informações dinamicamente preenchidas no formulário
 let form = document.querySelector('#form-user-create')
 
+function addLine(dataUser) {
+    let tr = document.createElement('tr')
+    document.querySelector('#table-users').appendChild(tr)
+
+    tr.innerHTML = `
+     <tr>
+                      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                      <td>${dataUser.name}</td>
+                      <td>${dataUser.email}</td>
+                      <td>${dataUser.admin}</td>
+                      <td>${dataUser.birth}</td>
+                      <td>
+                        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                      </td>
+                    </tr> 
+    `
+
+}
+
 
 console.log(fields)
 form.addEventListener('submit', (e) => {
@@ -25,5 +45,5 @@ form.addEventListener('submit', (e) => {
         }
     })
 
-    console.log(user)
+    addLine(user)
 })
